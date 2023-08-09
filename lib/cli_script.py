3edae9864f688b649b983e3ad10e500af02b2c2c
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-09 13:51:13 -0400

    implements alembic

--- lib/cli_script.py
+++ lib/cli_script.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+from models import Main
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///database.db')
+    engine = create_engine('sqlite:///lib/database.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
     print("Welcome to my CLI.")
 
-    import ipdb; ipdb.set_trace()
+    delete = session.query(Main).all()
+
+    for row in delete:
+        session.delete(row)
+    session.commit()
+    session.close()
+
+    # import ipdb; ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -3,3 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+class Main(Base):
+    __tablename__ = "main"
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    amount = Column(Integer())
