7cdd057a4ae9544a9c06a4815f135e4c7fcd9e43
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-09 13:19:51 -0400

    creates database file and installs ipdb and alembic

--- lib/cli_script.py
+++ lib/cli_script.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == '__main__':
+    engine = create_engine('sqlite:///database.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     print("Welcome to my CLI.")
+
+    import ipdb; ipdb.set_trace()
