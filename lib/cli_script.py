fb3043b4e2424414bec613590cefb210d3ca699e
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-11 14:01:13 -0400

    adds date column to Expense table and also formats it when printed

--- lib/cli_script.py
+++ lib/cli_script.py
@@ -4,7 +4,8 @@ from models import Expense
 from helpers import print_main_menu, option_1
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker   
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime   
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///database.db')
@@ -22,7 +23,7 @@ if __name__ == '__main__':
 
     print_main_menu()
     while True:
-        menu_option = input("Select option: ")
+        menu_option = input("Select menu option: ")
         if menu_option.isdigit() and menu_option > '0' and menu_option < '8':
             if menu_option == '1':
                 option_1(session, Expense)
@@ -40,7 +41,7 @@ if __name__ == '__main__':
                 print('Goodbye!')
                 break
         else:
-            print(f'{menu_option} is an invalid input. Please enter a valid menu option.')
+            print('Please enter a valid menu option.')
     
     session.commit()
     
--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, String, Integer, ForeignKey)
+from sqlalchemy import (Column, String, Integer, ForeignKey, DateTime)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -10,12 +10,16 @@ class Expense(Base):
     id = Column(Integer(), primary_key=True)
     title = Column(String())
     amount = Column(Integer())
+    date = Column(DateTime)
     category_id = Column(Integer(), ForeignKey('categories.id'))
 
     category = relationship('Category', back_populates='expenses')
+    
+    def format_date(self):
+        return self.date.strftime("%m/%y")
 
     def __repr__(self):
-        return f'Title: {self.title} | Amount: {self.amount} | Category: {self.category.name}'
+        return f'Title: {self.title} | Amount: {self.amount} | Category: {self.category.name} | Date: {self.format_date()}'
 
 class Category(Base):
     __tablename__ = "categories"
