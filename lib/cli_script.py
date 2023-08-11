a4383a33cf7c45973dc69daa419945cb0d5d093c
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-11 07:35:07 -0400

    implements menu and exit option

--- lib/cli_script.py
+++ lib/cli_script.py
@@ -10,11 +10,45 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    print('''Expense Tracker''')
+    print('''
+███████╗██╗░░██╗██████╗░███████╗███╗░░██╗░██████╗███████╗  ████████╗██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░
+██╔════╝╚██╗██╔╝██╔══██╗██╔════╝████╗░██║██╔════╝██╔════╝  ╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+█████╗░░░╚███╔╝░██████╔╝█████╗░░██╔██╗██║╚█████╗░█████╗░░  ░░░██║░░░██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝
+██╔══╝░░░██╔██╗░██╔═══╝░██╔══╝░░██║╚████║░╚═══██╗██╔══╝░░  ░░░██║░░░██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗
+███████╗██╔╝╚██╗██║░░░░░███████╗██║░╚███║██████╔╝███████╗  ░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚══════╝╚═╝░░╚══╝╚═════╝░╚══════╝  ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
+    ''')
 
-    expense = Expense()
-    # category = Category(name='Misc.')
-    # session.add(category)
+    while True:
+        print('''
+Enter option number and press 'enter' to select              
+1. View expenses
+2. Add expense
+3. Edit expenses
+4. Filter expenses
+5. Exit
+        ''')
+
+        menu_option = input()
+        if menu_option.isdigit() and menu_option > '0' and menu_option < '6':
+            if menu_option == '1':
+                pass
+            if menu_option == '2':
+                pass
+            if menu_option == '3':
+                pass
+            if menu_option == '4':
+                pass
+            if menu_option == '5':
+                print('Goodbye!')
+                break
+        else:
+            print(f'{menu_option} is an invalid input. Please enter valid menu option.')
+    
+    
+    # rent_category = session.query(Category).filter_by(name='Fun').first()
+    # expense = Expense(title='utilities', amount=200, category=rent_category)
+    # session.add(expense)
     session.commit()
     session.close()
 
