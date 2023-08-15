1d839a4b37d5ee82bdf42b850b655b2292604791
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-15 18:05:08 -0400

    changes 'filter' functionality into menu-based interface with dynamic menu generation based on existing entries

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import extract
+from sqlalchemy import extract, func
 from simple_term_menu import TerminalMenu
 from prettycli import red
 
@@ -19,11 +19,11 @@ def validate_input():
             print(red('PLEASE ENTER A WHOLE NUMBER (ROUNDED UP)'))
     category_id = 0
     categories = ["Fun", "Bills", "Food", "Misc."]
-    category_menu = TerminalMenu(categories, title="SELECT CATEGORY", menu_highlight_style=("bg_black", "fg_cyan"), menu_cursor_style=("fg_blue",))
+    category_menu = TerminalMenu(categories, title="SELECT CATEGORY:", menu_highlight_style=("bg_black", "fg_cyan"), menu_cursor_style=("fg_blue",))
     menu_entry_index = category_menu.show()
     category_id = menu_entry_index + 1
     
-    return [title, amount, category_id, datetime.strptime(date, "%m%d%y")]
+    return [title.capitalize(), amount, category_id, datetime.strptime(date, "%m%d%y")]
 
 def view_all(session, expense):
     results = session.query(expense).all()
@@ -69,30 +69,26 @@ def delete(session, expense, id):
     print('\nSUCCESSFULLY DELETED')
 
 def filter(session, expense):
-    pass
+    month_or_category = ["Month", "Category", "Cancel"]
+    filter_menu = TerminalMenu(month_or_category, title="FILTER BY:", menu_highlight_style=("bg_black", "fg_cyan"), menu_cursor_style=("fg_blue",))
+    index = filter_menu.show()
+    if month_or_category[index] == "Month":
+        results = session.query(expense).all()
+        months_set = set()
+        for result in results:
+            months_set.add(result.date.strftime("%B"))
+        month_options = list(months_set)
+        months_menu = TerminalMenu(month_options, menu_highlight_style=("bg_black", "fg_cyan"), menu_cursor_style=("fg_blue",))
+        month_index = months_menu.show()
+        filtered_results = [result for result in results if result.date.strftime("%B") == month_options[month_index]]
+        print(f"EXPENSES FOR MONTH OF: {month_options[month_index]}")
+        for filtered_result in filtered_results:
+            print(filtered_result)
+    elif month_or_category[index] == "Category":
+        pass
+    elif month_or_category[index] == "Cancel":
+        return
 
-    # print('FILTER BY: 1. Month 2. Category')
-    # while True:
-    #     filter = input('SELECT: ')
-    #     if filter.isdigit() and '1' <= filter <= '2':
-    #         break
-    #     else:
-    #         print(red('PLEASE ENTER VALID OPTION'))
-    # if filter == '1':
-    #     while True:
-    #         month = input('ENTER MONTH (MM): ')
-    #         if month.isdigit() and len(month) == 2:
-    #             month_int = int(month)
-    #             results = session.query(expense).filter(extract('month', expense.date) == month_int).all()
-    #             if results:
-    #                 for result in results:
-    #                     print(result)
-    #                 break
-    #             else: 
-    #                 print(f'NO RESULTS FOR MONTH: {month}')
-    #                 break
-    #         else:
-    #             print("PLEASE ENTER MONTH IN MM NUMERIC FORMAT")
     # elif filter == '2':
     #     while True:
     #         category = input('SELECT CATEGORY: 1. Fun 2. Bills 3. Food 4. Misc.: ')
