d826dea1f5440664e6e37491ae2c1a6859904732
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-08-17 09:10:03 -0400

    encapsulates filter menu into its own function

--- lib/helpers.py
+++ lib/helpers.py
@@ -2,6 +2,25 @@ from datetime import datetime
 from simple_term_menu import TerminalMenu
 from prettycli import red
 
+def filter_menu(results):
+    filter_options = ["Month", "Category", "Cancel"]
+    filter_menu = TerminalMenu(filter_options, title="FILTER BY:", menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
+    filter_menu_index = filter_menu.show()
+    if filter_options[filter_menu_index] == "Month":
+        months_set = set()
+        for result in results:
+            months_set.add(result.date.strftime("%B"))
+        month_options = sorted(list(months_set), key=lambda month: datetime.strptime(month, "%B"))
+        month_menu = TerminalMenu(month_options, menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
+        month_menu_index = month_menu.show()
+        return month_options[month_menu_index]
+    
+    elif filter_options[filter_menu_index] == "Category":
+        category_options = ["Fun", "Bills", "Food", "Misc."]
+        category_menu = TerminalMenu(category_options, title="SELECT CATEGORY:", menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
+        category_menu_index = category_menu.show()
+        return category_menu_index + 1
+    
 def valid_date(date_string):
     try:
         datetime.strptime(date_string, "%m%d%y")
@@ -38,6 +57,7 @@ def view_all(session, expense):
         expenses_menu = TerminalMenu(expenses_options, menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
         expenses_menu_index = expenses_menu.show()
         print(f'SELECTED: {expenses_options[expenses_menu_index]}')
+
         edit_delete_options = ["Edit", "Delete", "Cancel"]
         edit_delete_menu = TerminalMenu(edit_delete_options, menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
         edit_delete_menu_index = edit_delete_menu.show()
@@ -77,30 +97,19 @@ def delete(session, expense, id):
 def filter(session, expense):
     results = session.query(expense).order_by(expense.date).all()
     if results:
-        filter_options = ["Month", "Category", "Cancel"]
-        filter_menu = TerminalMenu(filter_options, title="FILTER BY:", menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
-        filter_menu_index = filter_menu.show()
-        if filter_options[filter_menu_index] == "Month":
-            months_set = set()
-            for result in results:
-                months_set.add(result.date.strftime("%B"))
-            month_options = sorted(list(months_set), key=lambda month: datetime.strptime(month, "%B"))
-            month_menu = TerminalMenu(month_options, menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
-            month_menu_index = month_menu.show()
-            filtered_results = [result for result in results if result.date.strftime("%B") == month_options[month_menu_index]]
-            print(f"EXPENSES FOR MONTH OF: {month_options[month_menu_index]}")
+        selection = filter_menu(results)
+        if type(selection) is int:
+            filtered_results = [result for result in results if result.category_id == selection]
             for item in filtered_results:
                 print(item)
-        elif filter_options[filter_menu_index] == "Category":
-            category_options = ["Fun", "Bills", "Food", "Misc."]
-            category_menu = TerminalMenu(category_options, title="SELECT CATEGORY:", menu_highlight_style=("bg_black", "fg_cyan", "bold"), menu_cursor_style=("fg_blue",))
-            category_menu_index = category_menu.show()
-            filtered_results = [result for result in results if result.category_id == category_menu_index + 1]
-            print(f"EXPENSES IN CATEGORY: {category_options[category_menu_index]}")
+        else:
+            filtered_results = [result for result in results if result.date.strftime("%B") == selection]
+            print(f"EXPENSES FOR MONTH OF: {selection}")
             for item in filtered_results:
                 print(item)
-        elif filter_options[filter_menu_index] == "Cancel":
-            return
     else:
         print(red('NO EXPENSES FOUND'))
+
+def aggregate(session, expense):
+    pass
                  
